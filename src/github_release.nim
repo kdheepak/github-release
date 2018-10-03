5cc7fe64720670e81ea69de0f6da88b3a0713023
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-03 00:55:08 -0600

    Add main stub

--- src/github_release.nim
+++ src/github_release.nim
@@ -1,6 +1,17 @@
-
+proc main(token: string, owner: string, repo: string, file: string, tag: string): int =
+    return 0
 
 
 when isMainModule:
 
-    echo "hello world"
+    import cligen
+    import os
+    const version_string = staticExec("git rev-parse --verify HEAD --short")
+
+    dispatchGen(main, version = ("version", "glm (v0.1.0-dev " & versionString & ")"))
+
+    if paramCount() == 0:
+        discard dispatch_main(@["--version"])
+        quit(dispatch_main(@["--help"]))
+    else:
+        quit(dispatch_main(commandLineParams()))
